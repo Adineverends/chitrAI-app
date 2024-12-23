23757f8c140c64838d546374a4c6ab972d1cf9fc
Adineverends <75876903+Adineverends@users.noreply.github.com>
2024-12-23 20:52:11 +0530

    Update SettingsPage.dart

--- lib/SettingsPage.dart
+++ lib/SettingsPage.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/cupertino.dart';
 
+import 'package:flutter/material.dart';
+
+void main() => runApp (MyApp());
+
+class MyApp extends StatelessWidget {
+
+@override Widget build(BuildContext context) {
+
+return CupertinoApp(
+
+debugShowCheckedModeBanner: false, home: SettingsPage(), );
+
+ار }
+
+class SettingsPage extends StatefulWidget {
+
+@override
+
+SettingsPageState createState() => SettingsPageState(); }
+
+class SettingsPageState extends State<SettingsPage> {
+
+bool isPushNotificationEnabled = true;
+
+bool isDarkModeEnabled false;
+
+bool isLocationEnabled = false;
+
+String username 'John Doe';
+
+String email = 'john.doe@example.com';
+
+@override
+
+Widget build(BuildContext context) { return CupertinoPageScaffold(
+
+navigationBar: CupertinoNavigationBar( middle: Text('Settings'),
+
+), child: SafeArea(
+
+child: ListView( children: [ // Profile Section padding: const EdgeInsets.all(16.0),
+
+Padding(
